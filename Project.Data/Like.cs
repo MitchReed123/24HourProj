523846ed6de3fc20684c58faf463d0ae4b9235d7
Nicholas <higherfuryx@mail.com>
2020-09-02 13:19:34 -0500

    Jacob fixed it

--- Project.Data/Like.cs
+++ Project.Data/Like.cs
@@ -1,6 +1,19 @@
-﻿namespace Project.Data
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Data
 {
     public class Like
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
     }
 }
